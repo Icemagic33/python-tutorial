f21d85856e70f1e8d8aa767c2a73e0dcbf16935a
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-30 21:51:37 +0900

    Create test_1.py

---
+++ c4/test_1.py
@@ -0,0 +1,13 @@
+score = float(input("Enter your test score: "))
+grade = ""
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+else:
+    grade = "F"
+
+print(grade)
